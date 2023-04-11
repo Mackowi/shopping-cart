0dd8aafb7b07ccd1130e93ba836c16b67297ad34
Mackowi <dzierzenga.maciej@gmail.com>
2023-04-11 20:25:51 +0200

    rectractable sidebar implement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import { FaBeer } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import Cart from './components/Cart';
+import './styles/styles.css'
 
 export default function App() {
+
+  const [isCartOpen, setIsCartOpen] = useState(false)
+
+  const handleCartToggle = () => {
+    console.log('gowno')
+    setIsCartOpen(!isCartOpen);
+  };
+
   return (
-    <div className="App">
-      App <FaBeer />
-      <div>
-        <Link to="/cart">Go to Cart</Link>
+    <div className="app">
+      App
+      <div className="cart-icon-container" onClick={handleCartToggle}>
+        <FaBeer />
       </div>
+      <Cart isCartOpen={isCartOpen} handleCartToggle={handleCartToggle} />
     </div>
   );
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-
-export default  function Cart() {
+export default function Cart({ isCartOpen, handleCartToggle }) {
+  
   return (
-  <div>Cart
-    <div>
-      <Link to="/">Go to homepage</Link>
+    <div className={`cart ${isCartOpen ? 'open' : ''}`}>
+      <div className="cart-icon" onClick={handleCartToggle}>
+        {/* Contents of the cart icon */}
+        Cart Icon
+      </div>
+      <div className="cart-contents">
+        {/* Contents of the shopping cart */}
+        <ul>
+          <li>Product 1</li>
+          <li>Product 2</li>
+          <li>Product 3</li>
+        </ul>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
