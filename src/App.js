d6a1c68c23fc290b304afc564efa9cfb93a7de1b
Mackowi <dzierzenga.maciej@gmail.com>
2023-04-11 19:41:22 +0200

    react icons implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import { FaBeer } from 'react-icons/fa';
 
 export default function App() {
   return (
     <div className="App">
       App
+      <h3> Lets go for a <FaBeer />? </h3>
     </div>
   );
 }
